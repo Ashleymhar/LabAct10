aae8c8464681e5ea049d371e7e246bea85b2d786
Ahsleymhar <bashleymhar@gmail.com>
2025-11-07 08:58:01 +0800

    Linagyan ng code

--- LabAct10/Form1.Designer.vb
+++ LabAct10/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,77 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.lstSorted = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(251, 126)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(276, 152)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(276, 181)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(276, 210)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(75, 23)
+        Me.btnSort.TabIndex = 3
+        Me.btnSort.Text = "Read Sort"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'lstSorted
+        '
+        Me.lstSorted.FormattingEnabled = True
+        Me.lstSorted.Location = New System.Drawing.Point(99, 126)
+        Me.lstSorted.Name = "lstSorted"
+        Me.lstSorted.Size = New System.Drawing.Size(120, 95)
+        Me.lstSorted.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstSorted)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnSort As Button
+    Friend WithEvents lstSorted As ListBox
 End Class
--- LabAct10/Form1.vb
+++ LabAct10/Form1.vb
@@ -1,3 +1,46 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private NumbersList As New List(Of Integer)
+
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        If IsNumeric(txtNumber.Text) Then
+            NumbersList.Add(CInt(txtNumber.Text))
+            lstSorted.Items.Add(txtNumber.Text)
+            txtNumber.Clear()
+        Else
+            MessageBox.Show("Please enter a valid number")
+        End If
+    End Sub
+
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        Dim writer As New StreamWriter("numbers.txt")
+
+        For Each num As Integer In NumbersList
+            writer.WriteLine(num)
+        Next
+
+        writer.Close()
+        MessageBox.Show("Numbers saved to file!")
+    End Sub
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+
+        lstSorted.Items.Clear()
+
+
+        Dim lines() As String = File.ReadAllLines("numbers.txt")
+
+
+        Dim sortedNumbers = lines.Select(Function(x) CInt(x)).OrderBy(Function(x) x)
+
+
+        For Each num As Integer In sortedNumbers
+            lstSorted.Items.Add(num)
+            Console.WriteLine(num)
+        Next
+    End Sub
 End Class
